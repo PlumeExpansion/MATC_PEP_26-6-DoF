dec8ac08ccb3e4fb071f9cfbb0794990e2f79c27
PlumeExpansion <plume.expansion@gmail.com>
2026-02-05 23:02:25 -0600

    Started Three.js visualization

---
+++ Model/visualization/vite.config.js
@@ -0,0 +1,14 @@
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+
+	server: {
+		port: 8800,
+		strictPort: true
+	},
+	preview: {
+		port: 8800,
+		strictPort: true
+	}
+}
